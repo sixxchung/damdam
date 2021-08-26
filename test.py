a19cb604f34d58d00b286f05e36cdda129020bf0
sixxchung <sixxchung@gmail.com>
2021-08-27 06:30:13 +0900

    first

---
+++ test.py
@@ -0,0 +1,8 @@
+import json # import json module
+
+# with statement
+PATH = './20210818080000-10.json'
+with open('json file path or name') as json_file:
+    json_data = json.load(json_file)
+
+print("sixx")
